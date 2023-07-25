8b2014a2637ac9c85f05c8fdb857e203c8426952
ZaibLComrade <kmjahanzaib@gmail.com>
2023-07-25 11:30:38 +0600

    Added simple Legal Practice Area Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
 		  'custom-black': '#414040',
 		  'custom-grey-1': '#707070',
 		  'custom-grey-2': '#E7E7E7',
+		  'custom-grey-3': '#F3F3F3',
 		  'custom-deer': '#B68C5A',
 		  'white': '#FFF'
 	  },
